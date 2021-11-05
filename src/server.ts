0f383738c265542a02f6b80bd333a098b4b8b263
Zihao Li <zl81@st-andrews.ac.uk>
2021-11-05 03:16:07 +0000

    Revert "Implement the very first version of testing 'GET /api/user/:username' endpoint, add it to a new branch instead"
    This reverts commit ffdbb0f08ff574e79a2c44903e5f1c36b663d9f9.

--- src/server.ts
+++ src/server.ts
@@ -36,31 +36,29 @@ function validateConfig() {
 
 export const config = validateConfig();
 
-if (process.env.NODE_ENV !== 'test') {
-    // start our server
-    server.listen(process.env['PORT'] || 5000, () => {
-        const port = (server.address() as AddressInfo).port;
+//start our server
+server.listen(process.env['PORT'] || 5000, () => {
+    const port = (server.address() as AddressInfo).port;
 
-        Logger.info(`Server started on ${port}! (environment: ${process.env['NODE_ENV'] || 'dev'})`);
-        Logger.info('Attempting connection with MongoDB service');
+    Logger.info(`Server started on ${port}! (environment: ${process.env['NODE_ENV'] || 'dev'})`);
+    Logger.info('Attempting connection with MongoDB service');
 
-        // TODO(alex): Try to load the current federations configuration from disk, but if we don't
-        //             have it, then we make a request to the supergroup info endpoint at:
-        //             --> https://gbs3.host.cs.st-andrews.ac.uk/cs3099-journals.json
+    // TODO(alex): Try to load the current federations configuration from disk, but if we don't
+    //             have it, then we make a request to the supergroup info endpoint at:
+    //             --> https://gbs3.host.cs.st-andrews.ac.uk/cs3099-journals.json
 
-        mongoose.connect(
-            process.env['MONGODB_CONNECTION_URI']!,
-            {
-                connectTimeoutMS: 30000,
-            },
-            (err: any) => {
-                if (err) {
-                    Logger.error(`Failed to connect to MongoDB: ${err.message}`);
-                    process.exit(1);
-                }
+    mongoose.connect(
+        process.env['MONGODB_CONNECTION_URI']!,
+        {
+            connectTimeoutMS: 30000,
+        },
+        (err: any) => {
+            if (err) {
+                Logger.error(`Failed to connect to MongoDB: ${err.message}`);
+                process.exit(1);
+            }
 
-                Logger.info('Established connection with MongoDB service');
-            },
-        );
-    });
-}
+            Logger.info('Established connection with MongoDB service');
+        },
+    );
+});
