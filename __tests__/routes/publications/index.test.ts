a51cf3c3db7be1fcc5afa92c7018c096d4139b51
Zihao Li <zl81@st-andrews.ac.uk>
2021-11-09 02:37:26 +0000

    Implement tests for POST /publication/

---
+++ __tests__/routes/publications/index.test.ts
@@ -0,0 +1,133 @@
+import { agent as supertest, Response } from 'supertest';
+import app from '../../../src/app';
+import User, { IUserDocument } from '../../../src/models/User';
+import * as errors from '../../../src/common/errors';
+import Publication from '../../../src/models/Publication';
+
+const request = supertest(app);
+
+describe('Publications endpoints testing', () => {
+    let owner: (IUserDocument & { _id: string | undefined; }) | null;
+    let collabo1: (IUserDocument & { _id: string | undefined; }) | null;
+    let collabo2: (IUserDocument & { _id: string | undefined; }) | null;
+    let ownerRes: Response;
+    let collabo1Res: Response;
+    let collabo2Res: Response;
+    
+    beforeAll(async () => {
+        async function createAndLogin(username: string): Promise<Response> {
+            const registerResponse = await request
+                .post('/auth/register')
+                .send({
+                    email: `${username}@email.com`,
+                    username: username,
+                    firstName: username,
+                    lastName: username,
+                    password: 'Passwordexample123!',
+                    about: `I am ${username}`,
+                    profilePictureUrl: 'https://nothing-to-show.com',
+                });
+            expect(registerResponse.status).toBe(201);
+
+            const loginResponse = await request.post('/auth/login').send({
+                username,
+                password: 'Passwordexample123!',
+            });
+            expect(loginResponse.status).toBe(200);
+
+            return loginResponse;
+        }
+
+        ownerRes = await createAndLogin('owner');
+        collabo1Res = await createAndLogin('collabo1');
+        collabo2Res = await createAndLogin('collabo2');
+        
+        owner = await User.findOne({ username: 'owner' });
+        collabo1 = await User.findOne({ username: 'collabo1' });
+        collabo2 = await User.findOne({ username: 'collabo2' });
+    });
+
+    afterAll(async () => {
+        const deleteOwner = await request.delete('/user/owner').auth(ownerRes.body.token, {type: 'bearer'});
+        const deleteCollabo1 = await request.delete('/user/collabo1').auth(collabo1Res.body.token, {type: 'bearer'});
+        const deleteCollabo2 = await request.delete('/user/collabo2').auth(collabo2Res.body.token, {type: 'bearer'});
+        expect(deleteOwner.status).toBe(200);
+        expect(deleteCollabo1.status).toBe(200);
+        expect(deleteCollabo2.status).toBe(200);
+    });
+
+    // Tests for POST /publication/
+
+    it("should create a publication", async () => {
+        const response = await request.post('/publication/').auth(ownerRes.body.token, {type: 'bearer'}).send({
+            revision: 'v1',
+            title: 'Test title',
+            name: 'Test name',
+            introduction: 'Introduction here',
+            collaborators: ['collabo1', 'collabo2'],
+        });
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Successfully submitted new publication.');
+        expect(response.body.publication.revision).toBe('v1');
+        expect(response.body.publication.title).toBe('Test title');
+        expect(response.body.publication.name).toBe('test name');
+        expect(response.body.publication.introduction).toBe('Introduction here');
+        expect(response.body.publication.collaborators).toEqual([collabo1!.id, collabo2!.id]);
+        expect(response.body.publication.owner).toBe(owner!.id);
+
+        const publication = await Publication.count({ name: 'test name', revision: 'v1' });
+        expect(publication).toBe(1);
+    });
+
+    it("should create a publication with the same name and different revision", async () => {
+        const response = await request.post('/publication/').auth(ownerRes.body.token, {type: 'bearer'}).send({
+            revision: 'v2',
+            title: 'Test title',
+            name: 'Test name',
+            introduction: 'Introduction here',
+            collaborators: ['collabo1', 'collabo2'],
+        });
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe('Successfully submitted new publication.');
+        expect(response.body.publication.revision).toBe('v2');
+
+        const publication = await Publication.count({ name: 'test name', revision: 'v2' });
+        expect(publication).toBe(1);
+    });
+
+    it("should not create a publication with a redundant name and revision", async () => {
+        const response = await request.post('/publication/').auth(ownerRes.body.token, {type: 'bearer'}).send({
+            revision: 'v1',
+            title: 'Test title',
+            name: 'test name',
+            introduction: 'Introduction here',
+            collaborators: ['collabo1', 'collabo2'],
+        });
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe(errors.PUBLICATION_FAILED);
+        expect(response.body.extra).toBe(errors.PUBLICATION_EXISTS);
+
+        const publicationNum = await Publication.count({ name: 'test name', revision: 'v1'});
+        expect(publicationNum).toBe(1);
+    });
+
+    it("should not create a publication with a non-existent collaborator", async () => {
+        const response = await request.post('/publication/').auth(ownerRes.body.token, {type: 'bearer'}).send({
+            revision: 'v1',
+            title: 'Test title',
+            name: 'Test name 2',
+            introduction: 'Introduction here',
+            collaborators: ['collabo1', 'collabo3'],
+        });
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe(errors.NON_EXISTENT_USER);
+    });
+
+    // Tests for GET /publication/:username/:name/:revision?/tree/:path(*)
+
+    // Tests for GET /publication/:username/
+
+    // Tests for GET /publication/:username/:name/revisions
+
+    // Tests for GET /publication/:username/:revision?
+});
--- src/server.ts
+++ src/server.ts
@@ -36,28 +36,30 @@ function validateConfig() {
 export const config = validateConfig();
 
 //start our server
-server.listen(process.env['PORT'] || 5000, () => {
-    const port = (server.address() as AddressInfo).port;
+if (process.env.NODE_ENV !== 'test') {
+    server.listen(process.env['PORT'] || 5000, () => {
+        const port = (server.address() as AddressInfo).port;
 
-    Logger.info(`Server started on ${port}! (environment: ${process.env['NODE_ENV'] || 'dev'})`);
-    Logger.info('Attempting connection with MongoDB service');
+        Logger.info(`Server started on ${port}! (environment: ${process.env['NODE_ENV'] || 'dev'})`);
+        Logger.info('Attempting connection with MongoDB service');
 
-    // TODO(alex): Try to load the current federations configuration from disk, but if we don't
-    //             have it, then we make a request to the supergroup info endpoint at:
-    //             --> https://gbs3.host.cs.st-andrews.ac.uk/cs3099-journals.json
+        // TODO(alex): Try to load the current federations configuration from disk, but if we don't
+        //             have it, then we make a request to the supergroup info endpoint at:
+        //             --> https://gbs3.host.cs.st-andrews.ac.uk/cs3099-journals.json
 
-    mongoose.connect(
-        process.env['MONGODB_CONNECTION_URI']!,
-        {
-            connectTimeoutMS: 30000,
-        },
-        (err: any) => {
-            if (err) {
-                Logger.error(`Failed to connect to MongoDB: ${err.message}`);
-                process.exit(1);
-            }
+        mongoose.connect(
+            process.env['MONGODB_CONNECTION_URI']!,
+            {
+                connectTimeoutMS: 30000,
+            },
+            (err: any) => {
+                if (err) {
+                    Logger.error(`Failed to connect to MongoDB: ${err.message}`);
+                    process.exit(1);
+                }
 
-            Logger.info('Established connection with MongoDB service');
-        },
-    );
-});
+                Logger.info('Established connection with MongoDB service');
+            },
+        );
+    });
+}
