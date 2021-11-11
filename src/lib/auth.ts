7f9303cb92cc7d241fe3a1ea3f64b998c3db4994
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2021-11-11 15:05:46 +0000

    Use generic API Error for error codes in responses

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,11 +10,11 @@
 
 import assert from 'assert';
 import express from 'express';
-import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
 import { ZodError } from 'zod';
 import Logger from '../common/logger';
 import { config } from '../server';
 import { IAuthHeaderSchema } from '../validators/auth';
+import jwt, { JwtPayload, TokenExpiredError } from 'jsonwebtoken';
 
 export interface TokenPayload {
     token: string;
