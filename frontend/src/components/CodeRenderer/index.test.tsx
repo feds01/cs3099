6fb1db8151e73ae1529a64dff910ec25a9574fe7
Charlotte Clark <mc360@st-andrews.ac.uk>
2022-03-16 14:48:37 +0000

    added tests for 3 components

--- frontend/src/components/CodeRenderer/index.test.tsx
+++ frontend/src/components/CodeRenderer/index.test.tsx
@@ -1,19 +1,22 @@
 import CodeRenderer from '.';
 import { mockPublication } from '../../test-utils/mocks/publication';
 import { mockUser } from '../../test-utils/mocks/user';
+import { mockReview } from '../../test-utils/mocks/review';
 import { getGetReviewIdMock } from '../../lib/api/reviews/reviews.msw';
 import renderWithWrapper from '../../test-utils/render';
 
 describe('CodeRenderer tests', () => {
-  it('ill figure out what this test does later', () => {
-      const mockedReview = getGetReviewIdMock().review;
+  it('Code is rendered', () => {
+      const mockedReview = mockReview();
       const { getByText } = renderWithWrapper(<CodeRenderer
-        contents={""}
-        filename={""}
+        contents={"let x = 5;"}
+        filename={"test.tsx"}
         lineNumbers={true}
         lineOffset={5}
         language={"typescript"}
         review={mockedReview}/>);
-
+      
+      expect(getByText("let x = 5;")).toBeInTheDocument();
+        
   })  
 })
