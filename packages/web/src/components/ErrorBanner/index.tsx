e255c8ff830563b407094aa88f1b2f179aeab525
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2022-03-29 16:17:00 +0100

    chore: fmt, lint and test

--- packages/web/src/components/ErrorBanner/index.tsx
+++ packages/web/src/components/ErrorBanner/index.tsx
@@ -12,12 +12,12 @@ export default function ErrorBanner({ message, errors }: Props): ReactElement {
     return (
         <Alert severity="error">
             <AlertTitle>Error</AlertTitle>
-            <strong style={{lineBreak: 'anywhere'}}>{message}</strong>
+            <strong style={{ lineBreak: 'anywhere' }}>{message}</strong>
             {typeof errors !== 'undefined' && Object.keys(errors).length > 0 && (
                 <ul>
                     {Object.entries(errors).map(([key, value]) => {
                         return (
-                            <li style={{lineBreak: 'anywhere'}}>
+                            <li style={{ lineBreak: 'anywhere' }}>
                                 <strong>{key}</strong>: {value.message}
                             </li>
                         );
